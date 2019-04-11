772e004d0c8762f42bb744a190956e8acf253b97
Satish Babariya <satish.babariya@gmail.com>
2019-04-11 09:46:19 +0530

    setup monaco-languageclient

--- Client/src/monaco-component.tsx
+++ Client/src/monaco-component.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-// import * as monaco from 'monaco-editor-core';
 
-const ReconnectingWebSocket = require('reconnecting-websocket');
+// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+
 import { listen, MessageConnection } from 'vscode-ws-jsonrpc';
+import { BaseLanguageClient, CloseAction, ErrorAction, createMonacoServices, createConnection } from 'monaco-languageclient';
+const ReconnectingWebSocket = require('reconnecting-websocket');
 
-import {
-  MonacoLanguageClient, CloseAction, ErrorAction,
-  MonacoServices, createConnection
-} from 'monaco-languageclient';
+require('monaco-editor/esm/vs/editor/editor.main');
 
 export class MonacoComponent extends React.Component {
     private editorContainer: React.RefObject<HTMLDivElement>;
-
     public languageId = 'json';
+
+    private editor: monaco.editor.IStandaloneCodeEditor;
 
     constructor(props: {}) {
         super(props);
@@ -20,50 +20,76 @@ export class MonacoComponent extends React.Component {
     }
 
     public createUrl(): string {
-      switch (this.languageId) {
-        case 'json':
-          return 'ws://localhost:3000/sampleServer';
-      }
+        switch (this.languageId) {
+            case 'json':
+                return 'ws://localhost:3000/sampleServer';
+            case 'typescript':
+                return 'your/language-server';
+        }
     }
-  
-    // public createLanguageClient(connection: MessageConnection): MonacoLanguageClient  {
-    //   return new MonacoLanguageClient ({
-    //     name: `${this.languageId.toUpperCase()} Client`,
-    //     clientOptions: {
-    //       // use a language id as a document selector
-    //       documentSelector: [this.languageId],
-    //       // disable the default error handler
-    //       errorHandler: {
-    //         error: () => ErrorAction.Continue,
-    //         closed: () => CloseAction.DoNotRestart
-    //       }
-    //     },
-    //     // create a language client connection from the JSON RPC connection on demand
-    //     connectionProvider: {
-    //       get: (errorHandler, closeHandler) => {
-    //         return Promise.resolve(createConnection(connection, errorHandler, closeHandler));
-    //       }
-    //     }
-    //   });
-    // }
-  
+
     public createWebSocket(socketUrl: string): WebSocket {
-      const socketOptions = {
-        maxReconnectionDelay: 10000,
-        minReconnectionDelay: 1000,
-        reconnectionDelayGrowFactor: 1.3,
-        connectionTimeout: 10000,
-        maxRetries: Infinity,
-        debug: false
-      };
-      return new ReconnectingWebSocket(socketUrl, undefined, socketOptions);
+        const socketOptions = {
+            maxReconnectionDelay: 10000,
+            minReconnectionDelay: 1000,
+            reconnectionDelayGrowFactor: 1.3,
+            connectionTimeout: 10000,
+            maxRetries: Infinity,
+            debug: false,
+        };
+        return new ReconnectingWebSocket(socketUrl, undefined, socketOptions);
+    }
+
+    public createLanguageClient(connection: MessageConnection, services: BaseLanguageClient.IServices): BaseLanguageClient {
+        return new BaseLanguageClient({
+            name: `${this.languageId.toUpperCase()} Client`,
+            clientOptions: {
+                // use a language id as a document selector
+                documentSelector: [this.languageId],
+                // disable the default error handler
+                errorHandler: {
+                    error: () => ErrorAction.Continue,
+                    closed: () => CloseAction.DoNotRestart,
+                },
+            },
+            services,
+            // create a language client connection from the JSON RPC connection on demand
+            connectionProvider: {
+                get: (errorHandler, closeHandler) => {
+                    return Promise.resolve(createConnection(connection, errorHandler, closeHandler));
+                },
+            },
+        });
     }
 
     componentDidMount() {
-      const editor = monaco.editor.create(this.editorContainer.current, {
+        const LANGUAGE_ID = 'json';
+        const MODEL_URI = 'inmemory://model.json';
+        const MONACO_URI = monaco.Uri.parse(MODEL_URI);
+
+        // register the JSON language with Monaco
+        monaco.languages.register({
+            id: LANGUAGE_ID,
+            extensions: ['.json', '.bowerrc', '.jshintrc', '.jscsrc', '.eslintrc', '.babelrc'],
+            aliases: ['JSON', 'json'],
+            mimetypes: ['application/json'],
+        });
+
+        // create the Monaco editor
+        const value = `{
+          "$schema": "http://json.schemastore.org/coffeelint",
+          "line_endings": "unix"
+        }`;
+        
+        this.editor = monaco.editor.create(this.editorContainer.current, {
+            model: monaco.editor.createModel(value, LANGUAGE_ID, MONACO_URI),
+            glyphMargin: true,
+            lightbulb: {
+                enabled: true,
+            },
             // theme: cucumberThemeId,
-            value: "function hello() {\n\talert('Hello world!');\n}",
-            language: 'javascript',
+            // value: "function hello() {\n\talert('Hello world!');\n}",
+            // language: this.languageId,
             // autoIndent: true,
             // contextmenu: false,
             // automaticLayout: true,
@@ -73,22 +99,22 @@ export class MonacoComponent extends React.Component {
             // scrollBeyondLastLine: false,
         });
 
-        // install Monaco language client services
-MonacoServices.install(editor);
+        
 
-// create the web socket
-// const url = this.createUrl();
-// const webSocket = this.createWebSocket(url);
-// listen({
-//   webSocket,
-//   onConnection: connection => {
-//     // create and start the language client
-//     // const languageClient = this.createLanguageClient(connection, services);
-//     // const disposable = languageClient.start();
-//     // connection.onClose(() => disposable.dispose());
-//   }
-// });
-
+        // create the web socket
+        const url = this.createUrl();
+        const webSocket = this.createWebSocket(url);
+        // listen when the web socket is opened
+        const services = createMonacoServices(this.editor);
+        listen({
+            webSocket,
+            onConnection: connection => {
+                // create and start the language client
+                const languageClient = this.createLanguageClient(connection, services);
+                const disposable = languageClient.start();
+                connection.onClose(() => disposable.dispose());
+            },
+        });
     }
 
     render() {
--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -1,131 +1,59 @@
-const path = require('path');
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const distDir = path.resolve(__dirname, "dist");
+const indexPath = path.resolve(__dirname, "index.html");
 
-const distDir = path.resolve(__dirname, 'dist');
-const indexPath = path.resolve(__dirname, 'index.html');
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 
-// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-
-// module.exports = {
-//     mode: 'development',
-//     entry: {
-//         app: './src/index.tsx',
-//     },
-//     output: {
-//         globalObject: 'self',
-//         filename: '[name].bundle.js',
-//         path: distDir,
-//     },
-//     resolve: {
-//         extensions: ['.js', '.json', '.ts', '.tsx', '.worker'],
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.tsx?$/,
-//                 loader: 'awesome-typescript-loader',
-//             },
-//             {
-//                 test: /\.css$/,
-//                 use: ['style-loader', 'css-loader'],
-//             },
-//         ],
-//     },
-//     plugins: [
-//         new MonacoWebpackPlugin(),
-//         new CopyWebpackPlugin([
-//             {
-//                 from: indexPath,
-//                 to: distDir,
-//             },
-//         ]),
-//         new HtmlWebpackPlugin({
-//             inject: true,
-//             template: indexPath,
-//         }),
-//     ],
-    
-// };
-
-// const path = require('path');
-const lib = path.resolve(__dirname, "lib");
-
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
-const common = {
-    entry: {
-        app: './src/index.tsx',
-        "editor.worker": 'monaco-editor-core/esm/vs/editor/editor.worker.js'
-    },
-        resolve: {
-        extensions: ['.js', '.json', '.ts', '.tsx', '.worker'],
-    },
-    output: {
-        filename: '[name].bundle.js',
-        path: distDir
-    },
-    module: {
-        rules: [
-        //     {
-        //     test: /\.css$/,
-        //     use: ['style-loader', 'css-loader']
-        // },
-                    {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-                    {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-            },
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.tsx"
+    // "editor.worker": 'monaco-editor/esm/vs/editor/editor.worker.js',
+  },
+  output: {
+    globalObject: "self",
+    filename: "[name].bundle.js",
+    path: distDir
+  },
+  resolve: {
+    // alias: {
+    //     'vscode': require.resolve('monaco-languageclient/lib/vscode-compatibility')
+    // },
+    extensions: [".js", ".json", ".ts", ".tsx", ".worker"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
     ]
-    },
-    target: 'web',
-    node: {
-        fs: 'empty',
-        child_process: 'empty',
-        net: 'empty',
-        crypto: 'empty'
-    },
-    resolve: {
-        alias: {
-            'vscode': require.resolve('monaco-languageclient/lib/vscode-compatibility')
-        }
-    }
-};
-
-if (process.env['NODE_ENV'] === 'production') {
-    module.exports = merge(common, {
-        plugins: [
-            new UglifyJSPlugin(),
-            new webpack.DefinePlugin({
-                'process.env.NODE_ENV': JSON.stringify('production')
-            }),
-                    new CopyWebpackPlugin([
-            {
-                from: indexPath,
-                to: distDir,
-            },
-        ]),
-        new HtmlWebpackPlugin({
-            inject: true,
-            template: indexPath,
-        }),
-        ]
-    });
-} else {
-    module.exports = merge(common, {
-        devtool: 'source-map',
-        module: {
-            rules: [{
-                test: /\.js$/,
-                enforce: 'pre',
-                loader: 'source-map-loader'
-            }]
-        }
+  },
+  plugins: [
+    new MonacoWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: indexPath,
+        to: distDir
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: indexPath
     })
-} 
+  ],
+  target: "web",
+  node: {
+    fs: "empty",
+    child_process: "empty",
+    net: "empty",
+    crypto: "empty"
+  }
+};
